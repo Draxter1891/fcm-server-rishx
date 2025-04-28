89a8bec3714d4ad9f4b7c3029bfddf12bcfff84b
Rishabh Tripathi <trishabh2001@gmail.com>
2025-04-28 16:25:55 +0530

    finalized fcm live server for RishX mobile app

---
+++ controllers/notifications.js
@@ -0,0 +1,67 @@
+const admin = require('../config/firebase')
+const User = require('../model/User')
+
+const sendNotification = async (req, res) => {
+    const { token, title, description, imageUrl } = req.body;
+
+    try {
+        const response = await admin.messaging().send({
+            token: token,
+            data: {
+                title: title,
+                description: description,
+                imageUrl: imageUrl
+            }
+        })
+
+        res.status(200).send(`Notification sent successfully🚀: ${JSON.stringify(response)}`)
+    } catch (error) {
+        res.status(500).send(`Error Sending Notification: ${error.message}`)
+    }
+}
+
+
+const broadcastNotification = async (req, res) => {
+    const { title, description, imageUrl } = req.body;
+
+    try {
+        const users = await User.find().select('device_token -_id');
+        const deviceTokens = users?.map((user) => user.device_token)
+        const response = await admin.messaging().sendEachForMulticast({
+            token: deviceTokens,
+            data: {
+                title: title,
+                description: description,
+                imageUrl: imageUrl
+            }
+        })
+
+        res.status(200).send(`Notification broadcast successfully🚀: ${JSON.stringify(response)}`)
+    } catch (error) {
+        res.status(500).send(`Error Broadcasting Notification: ${error.message}`)
+    }
+}
+
+const registerToken = async (req, res) => {
+    const { device_token } = req.body
+
+    try {
+        let user = await User.findOne({ device_token })
+        if (user) {
+            return res.status(200).send('Device token already registered')
+        }
+
+        user = new User({ device_token })
+
+        await user.save()
+        res.status(201).send('Device token registered successfully🚀')
+    } catch (error) {
+        res.status(500).send(`Error Registering Device Token : ${error.message}`)
+    }
+}
+
+module.exports = {
+    sendNotification,
+    broadcastNotification,
+    registerToken,
+}
